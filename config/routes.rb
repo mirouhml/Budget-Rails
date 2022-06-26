4b7c87e41543eee0653da6ccc47bbefd952b1657
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-26 22:52:53 +0100

    Fix and add tailwindcss.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
