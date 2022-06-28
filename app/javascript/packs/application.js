54c9466220222b2fede078b496ed2b784c0f4772
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-28 17:23:56 +0100

    Add fadeout option for flash messages.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
-console.log('Hello from application.js');
+console.log('Hello from application.js YES');
+
+window.setTimeout(
+  "document.getElementById('flash').classList.toggle('fade');",
+  2000
+);
