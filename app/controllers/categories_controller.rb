0083baab1b4c42744302642e9f5f9515c9682a7d
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-28 12:58:02 +0100

    Add create category functionality and view.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,11 +2,28 @@ class CategoriesController < ApplicationController
   load_and_authorize_resource
   def index
     @user = current_user
-    @categories = current_user.categories
+    @categories = current_user.categories.includes(:author)
   end
 
   def show; end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.author_id = current_user.id
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
